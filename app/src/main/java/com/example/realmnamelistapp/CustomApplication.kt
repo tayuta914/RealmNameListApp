6656006f5c54e61113675086206d23628d0aa563
tayuta914 <tayuta914@gmail.com>
2023-02-13 06:53:56 +0900

    Realm init

---
+++ app/src/main/java/com/example/realmnamelistapp/CustomApplication.kt
@@ -0,0 +1,18 @@
+package com.example.realmnamelistapp
+
+import android.app.Application
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+class CustomApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+
+        val config = RealmConfiguration.Builder()
+            .allowWritesOnUiThread(true)
+            .allowQueriesOnUiThread(true)
+            .build()
+            Realm.setDefaultConfiguration(config)
+    }
+}
